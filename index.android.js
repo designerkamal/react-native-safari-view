1f17ba56f77082526647295b52865513c8a12cdd
Kamal <designerkamal@gmail.com>
2016-06-30 15:57:05 +0530

    Update index.android.js

--- index.android.js
+++ index.android.js
@@ -7,8 +7,6 @@
 'use strict';
 
 var warning = require('warning');
-const {NativeModules} = require('react-native');
-const NativeSafariViewManager = NativeModules.SafariViewManager;
 
 var SafariViewManager = {
   test: function() {
@@ -16,13 +14,7 @@ var SafariViewManager = {
   },
   isAvailable: function() {
     return new Promise(function(resolve, reject) {
-      NativeSafariViewManager.isAvailable(function(error) {
-        if (error) {
-          return reject(error);
-        }
-
         resolve(false);
-      });
     });
   }
 };
